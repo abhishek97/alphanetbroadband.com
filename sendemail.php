05a27a3baff48a44475a0775f151ced0a04ce1e5
Abhishek <zapper.awake@gmail.com>
2016-01-06 17:21:14 +0530

    fix sendmail

--- sendemail.php
+++ sendemail.php
@@ -6,14 +6,13 @@ $message    = @trim(stripslashes($_POST['message']));
 $to   		= 'admin@alphanetbroadband.com';//replace with your email
 
 $headers   = array();
-$headers[] = "MIME-Version: 1.0\r\n";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1\r\n";
-$headers[] = "From: {$name} <{$from}>\r\n";
-$headers[] = "Reply-To: <{$from}>\r\n";
-$headers[] = "Subject: {$subject}\r\n";
-$headers[] = "X-Mailer: PHP/".phpversion()."\r\n";
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+$headers[] = "From: {$name} <{$from}>";
+$headers[] = "Reply-To: <{$from}>";
+$headers[] = "Subject: {$subject}";
+$headers[] = "X-Mailer: PHP/".phpversion();
 
-mail($to, $subject, $message, implode("\r\n",$headers) );
-
+mail($to, $subject, $message,  implode("\r\n", $headers) );
 
 die;
